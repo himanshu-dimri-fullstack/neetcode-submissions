2ab023c873de190a4d19af53055196e910899907
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-07-20 18:08:47 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int l = 0;
+        int r = 1;
+        int maxi = 0;
+        while(r<n){
+            int profit = prices[r]-prices[l];
+            maxi = max(maxi,profit);
+            while(profit<=0){
+                if(l==r){
+                    break;
+                }
+                l++;
+                profit = prices[r]-prices[l];
+            }
+           r++;
+        }
+        return maxi;
+    }
+};
